e3ad48b6616bc7f3c625ed4e007284d5788096d8
kitkatkandybar <75346228+kitkatkandybar@users.noreply.github.com>
2020-12-06 17:23:49 -0500

    Add files via upload

--- Dinosaur Game YT/Module/vga.v
+++ Dinosaur Game YT/Module/vga.v
@@ -3,17 +3,11 @@
 //This script is responsible for generating the vertical and horizontal addresses and timing pulses
 
 module vga(
-    input wire clk,
-    input wire reset_btn,
-    input btn_press,
+    input clk,
     output reg [9:0] vaddress,haddress,
     output reg vsync,hsync
     );
-    
-    wire reset = reset_btn;
-    wire animate;
-    wire [11:0] aster_x1, aster_x2, aster_y1, aster_y2;
-    
+
     //Initialize values
     initial begin
         vsync <= 1'b1;
@@ -21,25 +15,7 @@ module vga(
         vaddress <= 9'b0;
         haddress <= 9'b0;
     end
-    
-    top #(
-        .in_x(160), 
-        .in_y(300), 
-        .hSize(40)
-        ) 
-        asteroid(
-        .clk(clk), 
-        //.i_ani_stb(pixel_strobe),
-        .reset(reset),
-        .animate(animate),
-        .button_pressed(btn_press),
-        .is_dino(1),
-        .x1(aster_x1),
-        .x2(aster_x2),
-        .y1(aster_y1),
-        .y2(aster_y2)
-    );
-    
+
     //Main block, generates vertical and horizontal addresses and timing pulses
     always@(posedge clk)begin
         hsync <= 1'b1;
@@ -59,4 +35,4 @@ module vga(
             end
         end
     end //End of main block
-endmodule
+endmodule 
