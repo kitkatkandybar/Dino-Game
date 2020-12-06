21f8ec73f1928643c7fb6cce3a3af8f9bf0a858c
htgold1999 <57266425+htgold1999@users.noreply.github.com>
2020-12-06 18:35:26 -0500

    Update asteroid_move.v

--- Dinosaur Game YT/Module/asteroid_move.v
+++ Dinosaur Game YT/Module/asteroid_move.v
@@ -21,6 +21,51 @@
 
 
 module asteroid_move(
-
+    input clk,halt,reset,asteroid_on,
+    output reg [9:0] xmovaddr,
+    output reg [9:0] ymovaddr
     );
-endmodule
+    
+    //Initialize jump y data file
+//    reg [6:0] jumping [50:0];
+    
+    //Initialize internal regs
+    reg [17:0] count1;
+   
+    //reg bool;
+    
+    //Initializing regs and loading the y data into memory
+    initial begin
+       count1<=0;
+       xmovaddr <= 0;
+        ymovaddr <= 0;
+        //bool <= 0;
+//        $readmemb("jump.mem", jumping);
+    end
+    
+    //Main block
+    always@(posedge clk)begin
+        if(reset == 1)begin
+            count1 <= 0;
+            xmovaddr <= 0;
+            ymovaddr <= 0;
+        end
+        if(halt == 0)begin
+        if(asteroid_on==1)begin
+                count1 <= count1 + 1;
+                if(count1 == 251250)begin
+                    count1 <= 0;
+                    xmovaddr <= xmovaddr+1;
+                    ymovaddr <= ymovaddr+1;
+                end
+            end
+               else begin
+                xmovaddr <= 0;
+                ymovaddr <= 0;
+    
+                    end
+                end
+            end
+
+    
+ 
