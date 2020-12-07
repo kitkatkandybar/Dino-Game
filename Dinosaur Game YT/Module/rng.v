44cead7af5346e6bcacc3e0aec2cfaebaceebd84
htgold1999 <57266425+htgold1999@users.noreply.github.com>
2020-12-06 23:24:38 -0500

    Update rng.v

--- Dinosaur Game YT/Module/rng.v
+++ Dinosaur Game YT/Module/rng.v
@@ -6,20 +6,26 @@ module rng(
     input clk,button,
     output reg [4:0] random1
     );
-    
+      reg [17:0] count1;
     //Set initial values
     initial begin
         random1 <= 5'b1;
+        count1<=0;
     end
-    
+    always @(posedge clk) begin
+          counter1=counter1+1;
+    end
     //Main block, generates a pseudo-random output
     always@(posedge clk)begin
-        if(button)begin //If the jump button is pressed then increment this pseudo-random counter
+        
+        if(reset)begin //If the jump button is pressed then increment this pseudo-random counter
+            if (counter== (5*251250)) begin
             random1[0] <= random1[1]^random1[4];
             random1[1] <= random1[0];
             random1[2] <= random1[1];
             random1[3] <= random1[2];
             random1[4] <= random1[3];
+        end
         end
     end
 endmodule
