64bbdca999e8da9069bf7c6747b867299dcf8fbc
kitkatkandybar <75346228+kitkatkandybar@users.noreply.github.com>
2020-12-08 13:17:59 -0500

    Update asteroid_move.v

--- Dinosaur Game YT/Module/asteroid_move.v
+++ Dinosaur Game YT/Module/asteroid_move.v
@@ -22,28 +22,23 @@
 
 module asteroid_move (
     input clk,halt,reset,asteroid_on,
-    input[2:0] new_count, //I CHANGED THIS
+    input[2:0] new_count, 
     output reg [8:0] xmovaddr,
     output reg [9:0] ymovaddr
     );
     
-    //Initialize jump y data file
-//    reg [6:0] jumping [50:0];
-    
     //Initialize internal regs
     reg [17:0] count1;
-    reg [2:0] count2; //I CHANGED THIS
+    reg [2:0] count2; 
     reg count_val = 0;
     //reg bool;
     
     //Initializing regs and loading the y data into memory
     initial begin
-       count1<=0;
-       count2 <= new_count; //I CHANGED THIS, BUT THIS DOESN'T DO WHAT I HOPED
-       xmovaddr <= 0;
+        count1<=0;
+        count2 <= new_count;
+        xmovaddr <= 0;
         ymovaddr <= 0;
-        //bool <= 0;
-//        $readmemb("jump.mem", jumping);
     end
     
     //Main block
@@ -70,13 +65,12 @@ module asteroid_move (
             count_val = 1;
         end
         
-        if (xmovaddr == 0 && ymovaddr == 0) begin //I CHANGED THIS
+        if (xmovaddr == 0 && ymovaddr == 0) begin 
         
-            count2 = count2 + 1; //I CHANGED THIS
-            if (count2 == 6) begin //I CHANGED THIS
-                count2 = 0; //I CHANGED THIS
-            end //I CHANGED THIS
-            
+            count2 = count2 + 1; 
+            if (count2 == 6) begin
+                count2 = 0;
+            end
         
         end
        
@@ -87,13 +81,13 @@ module asteroid_move (
         end
         if(halt == 0)begin
         if(asteroid_on==1)begin
-                if (count2 == 0) begin //I CHANGED THIS
-                count1 <= count1 + 1; //I CHANGED THIS
+                if (count2 == 0) begin 
+                count1 <= count1 + 1; 
                 if(count1 == 251250)begin
                     count1 <= 0;
                     xmovaddr <= xmovaddr+1;
                     ymovaddr <= ymovaddr+1;
-                end //I CHANGED THIS
+                end 
                 end
                 if (count2 == 1) begin
                 count1 <= count1 + 1;
@@ -103,7 +97,7 @@ module asteroid_move (
                     ymovaddr <= ymovaddr+3;
                 end
                 end
-                if (count2 == 2) begin //I CHANGED THIS FROM HERE
+                if (count2 == 2) begin 
                 count1 <= count1 + 1;
                 if(count1 == 251250)begin
                     count1 <= 0;
@@ -138,8 +132,8 @@ module asteroid_move (
                     ymovaddr <= ymovaddr+2;
                 end
                 
-                end //TO HERE
-            end
+                end 
+                end
                else begin
                 xmovaddr <= 0;
                 ymovaddr <= 0;
