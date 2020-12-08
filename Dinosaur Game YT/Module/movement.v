c9436753f131da764d83d8407122047e768afc4a
jackhalb <56043640+jackhalb@users.noreply.github.com>
2020-12-08 13:16:25 -0500

    Update movement.v
    Removed comments

--- Dinosaur Game YT/Module/movement.v
+++ Dinosaur Game YT/Module/movement.v
@@ -1,42 +1,32 @@
 `timescale 1ns / 1ps
 
-//This script is responsible for generating the height data for the jumping dinosaur
+//This script is responsible for generating the movement of the dinosaur.
 
 module movement(
-    input clk,button,halt,reset,gs,//I CHANGED THIS
+    input clk,button,halt,reset,gs,
     output reg [9:0] movaddr
     );
-    
-    //Initialize jump y data file
-//    reg [6:0] jumping [50:0];
     
     //Initialize internal regs
     reg [17:0] millisecond;
     reg [8:0] second;
-    //reg bool;
     
-    //Initializing regs and loading the y data into memory
     initial begin
         millisecond <= 0;
         second <= 0;
-        //bool <= 0;
-//        $readmemb("jump.mem", jumping);
     end
     
     //Main block
     always@(posedge clk)begin
-        if (gs == 0) //I CHANGED THIS
-            movaddr = 0; //I CHANGED THIS
+        if (gs == 0)
+            movaddr = 0;
         if(reset == 1)begin
             millisecond <= 0;
             second <= 0;
             //bool <= 0;
         end
         if(halt == 0)begin
-//            if(button == 1 && bool == 0)begin //Check for jump button status
-//                bool <= 1;
-//            end
-            if(button == 1)begin //If the button has been pressed then poll y value from file every 10 milliseconds
+            if(button == 1)begin
                 millisecond <= millisecond + 1;
                 if(millisecond == 251250)begin
                     millisecond <= 0;
@@ -48,10 +38,6 @@ module movement(
                     movaddr <= movaddr + 1; 
                 end
             end
-//            else begin
-//                movaddr <= 0;
-//            end
-            //Assign output to the current line in the y position data file
         end
     end //end of main block
     
